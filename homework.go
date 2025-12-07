677bd1e5b05f57baa0648b13ae7c5afee1214a30
ZhuzaiBro <2981199851@qq.com>
2025-12-08 02:07:28 +0800

    完成作业

--- homework.go
+++ homework.go
@@ -3,57 +3,171 @@ package homework01
 // 1. 只出现一次的数字
 // 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
 func SingleNumber(nums []int) int {
-	// TODO: implement
-	return 0
+	// 使用异或运算，相同的数字异或结果为0，0与任何数异或结果为该数本身
+	result := 0
+	for _, num := range nums {
+		result ^= num
+	}
+	return result
 }
 
 // 2. 回文数
 // 判断一个整数是否是回文数
 func IsPalindrome(x int) bool {
-	// TODO: implement
-	return false
+	// 负数不是回文数，个位数为0的非0数字也不是回文数
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+
+	// 反转后半部分数字
+	reversed := 0
+	for x > reversed {
+		reversed = reversed*10 + x%10
+		x /= 10
+	}
+
+	// 当数字长度为奇数时，可以通过 reversed/10 去除中间的数字
+	return x == reversed || x == reversed/10
 }
 
 // 3. 有效的括号
 // 给定一个只包括 '(', ')', '{', '}', '[', ']' 的字符串，判断字符串是否有效
 func IsValid(s string) bool {
-	// TODO: implement
-	return false
+	// 使用栈来匹配括号
+	stack := []rune{}
+	pairs := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+
+	for _, ch := range s {
+		if ch == '(' || ch == '{' || ch == '[' {
+			// 左括号入栈
+			stack = append(stack, ch)
+		} else {
+			// 右括号，检查栈顶是否匹配
+			if len(stack) == 0 || stack[len(stack)-1] != pairs[ch] {
+				return false
+			}
+			stack = stack[:len(stack)-1] // 出栈
+		}
+	}
+
+	return len(stack) == 0
 }
 
 // 4. 最长公共前缀
 // 查找字符串数组中的最长公共前缀
 func LongestCommonPrefix(strs []string) string {
-	// TODO: implement
-	return ""
+	if len(strs) == 0 {
+		return ""
+	}
+
+	// 以第一个字符串为基准
+	prefix := strs[0]
+
+	for i := 1; i < len(strs); i++ {
+		// 不断缩短前缀，直到匹配
+		for len(prefix) > 0 && len(strs[i]) < len(prefix) ||
+			len(prefix) > 0 && strs[i][:len(prefix)] != prefix {
+			prefix = prefix[:len(prefix)-1]
+		}
+		if prefix == "" {
+			return ""
+		}
+	}
+
+	return prefix
 }
 
 // 5. 加一
 // 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一
 func PlusOne(digits []int) []int {
-	// TODO: implement
-	return nil
+	// 从最后一位开始加一
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		// 当前位是9，变成0，继续进位
+		digits[i] = 0
+	}
+
+	// 所有位都是9的情况，需要在最前面加1
+	return append([]int{1}, digits...)
 }
 
 // 6. 删除有序数组中的重复项
 // 给你一个有序数组 nums ，请你原地删除重复出现的元素，使每个元素只出现一次，返回删除后数组的新长度。
 // 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
 func RemoveDuplicates(nums []int) int {
-	// TODO: implement
-	return 0
+	if len(nums) == 0 {
+		return 0
+	}
+
+	// 双指针法：slow指向不重复元素的最后位置，fast遍历数组
+	slow := 0
+	for fast := 1; fast < len(nums); fast++ {
+		if nums[fast] != nums[slow] {
+			slow++
+			nums[slow] = nums[fast]
+		}
+	}
+
+	return slow + 1
 }
 
 // 7. 合并区间
 // 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
 // 请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
 func Merge(intervals [][]int) [][]int {
-	// TODO: implement
-	return nil
+	if len(intervals) == 0 {
+		return [][]int{}
+	}
+
+	// 按区间起始位置排序
+	for i := 0; i < len(intervals)-1; i++ {
+		for j := i + 1; j < len(intervals); j++ {
+			if intervals[i][0] > intervals[j][0] {
+				intervals[i], intervals[j] = intervals[j], intervals[i]
+			}
+		}
+	}
+
+	result := [][]int{intervals[0]}
+
+	for i := 1; i < len(intervals); i++ {
+		last := result[len(result)-1]
+		current := intervals[i]
+
+		if current[0] <= last[1] {
+			// 有重叠，合并区间
+			if current[1] > last[1] {
+				last[1] = current[1]
+			}
+		} else {
+			// 无重叠，添加新区间
+			result = append(result, current)
+		}
+	}
+
+	return result
 }
 
 // 8. 两数之和
 // 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数
 func TwoSum(nums []int, target int) []int {
-	// TODO: implement
+	// 使用哈希表存储已遍历的数字及其索引
+	numMap := make(map[int]int)
+
+	for i, num := range nums {
+		complement := target - num
+		if idx, found := numMap[complement]; found {
+			return []int{idx, i}
+		}
+		numMap[num] = i
+	}
+
 	return nil
 }
